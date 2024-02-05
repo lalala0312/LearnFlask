4d45dfc1dc027ed3f4f659d2eb928f5f1944893b
Leics <1503647333@qq.com>
2024-02-05 18:04:36 +0800

    first commite the project, just demo

---
+++ myproject/hello.py
@@ -0,0 +1,45 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+from flask import url_for
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
